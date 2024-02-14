874d41a01a59e55346336c73c99aaba04f5986fa
аврора <@LAPTOP-P3VCLJ88>
2024-02-14 14:47:48 +0300

    Добавьте файлы проекта.

---
+++ popii.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout << "Введите a:" << endl;
+    cin >> a;
+    cout << "Введите b:" << endl;
+    cin >> b;
+    c = a + b;
+    cout << "a+b=" << c;
